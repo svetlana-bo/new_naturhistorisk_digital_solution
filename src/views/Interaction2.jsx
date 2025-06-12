6fedb197cac7c28822e7fb0e9f7fefa8b6df5b2f
svetlana-bo <eaa24svbo@students.eaaa.dk>
2025-06-12 20:39:54 +0200

    ready for testing

--- src/views/Interaction2.jsx
+++ src/views/Interaction2.jsx
@@ -8,6 +8,7 @@ import birdFemaleImage from '../assets/images/female_birds/female_bird2.svg'
 
 import styles from '../modules/Interaction.module.css';
 
+import Interaction3 from './Interaction3.jsx';
 
 function Interaction2() {
   // Tracks if the player has "won" (i.e. attracted the female bird)
@@ -16,12 +17,21 @@ function Interaction2() {
    // Increments to trigger a reset animation in the female bird
   const [resetTrigger, setResetTrigger] = useState(0);
 
-   // Called when the win overlay video finishes playing
-  const handleResetDone = () => {
-    setWon(false);                      //  reset female attraction (to be able to start over)
-    setResetTrigger((n) => n + 1);      //  trigger female bird idle animation
-  };
+    // NEW: Tracks whether we should move to the next interaction view
+    const [showNextView, setShowNextView] = useState(false);
 
+    // Called when the win overlay video finishes playing
+    const handleResetDone = () => {
+      setWon(false);                      // Reset the win state
+      setResetTrigger((n) => n + 1);      // Trigger the female bird's idle animation
+      setShowNextView(true);              // Switch to next view
+    };
+  
+    // --- Render logic ---
+    if (showNextView) {
+      // If the win video has ended, show the next view
+      return <Interaction3 />;
+    }
   return (
     <div>
       <Title />
